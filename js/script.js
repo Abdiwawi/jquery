64233b9733b176bbb2354cc98c5fcd3eb3c219e8
Abdiwawi <abdijamzilan@gmail.com>
2017-04-04 19:34:26 +0300

    add javascript

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+  $("p").click(function() {
+    $("img").show();
+  });
+});
+
+$(document).ready(function() {
+        $("clickable").click(function() {
+          $(".walrus-showing").toggle();
+          $(".walrus-hidden").toggle();
+        });
+      });
